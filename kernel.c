24bd96ba3bb562f61cf71457bd1738e6a873a6d3
Riley <rileyleong24@gmail.com>
2024-07-17 22:19:25 -0700

    design loop to read parse and exectue commands, func decleration

--- kernel.c
+++ kernel.c
@@ -1,5 +1,22 @@
 #include <cstdlib>
 
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_exectue(args);
+
+        free(line);
+        free(args);
+    }   while (status);
+}
+
 int main(int argc, char **argv)
 {
     //Load config files
